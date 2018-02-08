b5620d0014284a36662a9904545f829e2086de06
Sridhar Vinnakota <sridharvinnakota@sridhars-mbp.attlocal.net>
2018-02-07 21:00:37 -0500

    Initial Commit

---
+++ src/main/java/kafka/Kafka_Producer.java
@@ -0,0 +1,36 @@
+package kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Kafka_Producer {
+
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+		
+		// kafka bootstrap server
+		properties.setProperty("bootstrap.servers", "127.0.0.1:9092");		
+		properties.setProperty("key.serializer", StringSerializer.class.getName());		
+		properties.setProperty("value.serializer", StringSerializer.class.getName());
+		
+		// producer acks
+		properties.setProperty("acks", "1");
+		properties.setProperty("retries", "3");
+		properties.setProperty("linger.ms", "1");
+		
+		Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+		
+		for(int key=0;key<10;key++)
+		{
+			ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("test", Integer.toString(key), "Has key "+Integer.toString(key));	
+			producer.send(producerRecord);
+		}
+		producer.close();
+		
+
+	}
+
+}
